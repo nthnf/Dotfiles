9faa06dceea53a83b2103376d7191d311bd6ff6c
Nathan Farrel <nathanfarrel1128@gmail.com>
2026-06-25 13:00:12 +0700

    chore: add Cachix binary caches and trust @nthnf group in nix.settings

--- modules/hosts/nfx/configuration.nix
+++ modules/hosts/nfx/configuration.nix
@@ -126,10 +126,26 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "26.05"; # Did you read the comment?
 
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
+  nix.settings = {
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+    substituters = [
+      "https://cache.nixos.org"
+      "https://niri.cachix.org"
+      "https://noctalia.cachix.org"
+    ];
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
+      "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
+    ];
+    trusted-users = [
+      "root"
+      "@nthnf"
+    ];
+  };
 
   stylix = {
     enable = true;
